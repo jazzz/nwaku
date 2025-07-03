19df60b41879bd79cfcd7c9ad977cb2149419040
fryorcraken <commits@fryorcraken.xyz>
2025-07-03 15:39:49 +1000

    wip

--- library/libwaku_conf.nim
+++ library/libwaku_conf.nim
@@ -1,3 +1,5 @@
+import std/options, waku/factory/waku_conf, waku/factory/conf_builder
+
 type ShardingMode* = enum
   AutoSharding
   StaticSharding
@@ -20,3 +22,45 @@ type LibWakuConf* = object
   mode*: WakuMode
   networkConf*: NetworkConf
   storeConfirmation*: bool
+
+proc toWakuConf*(libConf: LibWakuConf): Result[WakuConf, string] =
+  let b = WakuConfBuilder.init()
+  
+  case mode
+  of Relay:
+    b.withRelayEnabled(true)
+    b.filterServiceConf.withEnabled(true)
+    b.lightPushServiceConf.withEnabled(true)
+    b.withDiscv5Enabled(true)
+    b.withPeerExchange(true)
+
+    # Values applied when used as a library - should probably become default values
+    b.filterServiceConf.withMaxPeersToServe(20)
+    b.withRateLimits(
+      @[
+        
+      ]
+    )
+nwakuCfg.RateLimits.Filter = &bindingscommon.RateLimit{Volume: 100, Period: 1, TimeUnit: bindingscommon.Second}
+		nwakuCfg.RateLimits.Lightpush = &bindingscommon.RateLimit{Volume: 5, Period: 1, TimeUnit: bindingscommon.Second}
+    nwakuCfg.RateLimits.PeerExchange = &bindingscommon.RateLimit{Volume: 5, Period: 1, TimeUnit: bindingscommon.Second}
+
+
+  of Edge:
+    return err("Edge mode is not implemented")
+  
+
+
+# 	if !cfg.LightClient {
+		nwakuCfg.Discv5Discovery = true
+		nwakuCfg.Relay = true
+		nwakuCfg.Filter = true
+		nwakuCfg.FilterMaxPeersToServe = 20
+		nwakuCfg.Lightpush = true
+		
+	}
+
+	if cfg.EnablePeerExchangeServer {
+		nwakuCfg.PeerExchange = true
+		
+	}
